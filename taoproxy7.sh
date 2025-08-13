ff5d923465386b018f03a93da9919338ca6b17c1
Frozen <phuvipro618@gmail.com>
2025-08-14 03:52:02 +0700

    Update and rename taoproxy6.sh to taoproxy7.sh

---
+++ taoproxy7.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+set -e
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+WORKDIR="/home/cloudfly"
+WORKDATA="${WORKDIR}/data.txt"
+
+# Tạo folder WORKDIR
+sudo mkdir -p "$WORKDIR"
+sudo chown $USER:$USER "$WORKDIR"
+cd "$WORKDIR"
+
+# Hàm sinh password ngẫu nhiên
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c12
+    echo
+}
+
+# Tạo IPv6 ngẫu nhiên
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+# Cài 3proxy
+install_3proxy() {
+    echo "Installing 3proxy..."
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    sudo mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    sudo cp src/3proxy /usr/local/etc/3proxy/bin/
+    cd "$WORKDIR"
+}
+
+# Tải file proxy
+download_proxy() {
+    if [[ -f proxy.txt ]]; then
+        curl -F "file=@proxy.txt" https://file.io
+    else
+        echo "proxy.txt not found!"
+    fi
+}
+
+# Sinh dữ liệu proxy
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "user$port/$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+# Tạo file proxy.txt
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
+
+# Cài đặt các gói cần thiết
+sudo apt update -y
+sudo apt install -y wget gcc net-tools bsdtar zip curl
+
+# Lấy IP
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal IP = ${IP4}, External subnet for IPv6 = ${IP6}"
+
+# Nhập port
+while :; do
+  read -p "Enter FIRST_PORT between 21000 and 61000: " FIRST_PORT
+  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((FIRST_PORT >= 21000 && FIRST_PORT <= 61000)); then
+    break
+  else
+    echo "Number out of range, try again"
+  fi
+done
+LAST_PORT=$(($FIRST_PORT + 750))
+echo "LAST_PORT is $LAST_PORT. Continue..."
+
+# Sinh dữ liệu và các file config
+gen_data >"$WORKDATA"
+
+# Gen ifconfig script
+awk -F "/" '{print "sudo ip -6 addr add "$5"/64 dev eth0"}' "$WORKDATA" >"$WORKDIR/boot_ifconfig.sh"
+chmod +x "$WORKDIR/boot_ifconfig.sh"
+
+# Cài 3proxy
+install_3proxy
+
+# Gen 3proxy config
+awk -F "/" 'BEGIN{print "daemon\nmaxconn 2000\nauth strong"} {print "users "$1":CL:"$2"\nproxy -6 -n -a -p"$4" -i"$3" -e"$5"\nflush"}' "$WORKDATA" \
+    > /usr/local/etc/3proxy/3proxy.cfg
+sudo chmod 644 /usr/local/etc/3proxy/3proxy.cfg
+
+# Chạy ifconfig và 3proxy
+bash "$WORKDIR/boot_ifconfig.sh"
+sudo /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
+
+# Gen proxy file
+gen_proxy_file_for_user
+
+# Upload proxy.txt
+download_proxy
+
+echo "3proxy setup completed!"
